62bfa3d41d234dc52ca34470c45347a5717c6387
Kybxd <627940450@qq.com>
2025-03-19 14:38:34 +0800

    feat: notes

--- pkg/treemap/treemap.go
+++ pkg/treemap/treemap.go
@@ -78,14 +78,11 @@ func (m *TreeMap[K, V]) Max() (key K, value V, ok bool) {
 }
 
 // Floor finds the floor key-value pair for the input key.
-// In case that no floor is found, then both returned values will be nil.
-// It's generally enough to check the first value (key) for nil, which determines if floor was found.
+// In case that no floor is found, then both returned values will be corresponding type's empty value.
 //
-// Floor key is defined as the largest key that is smaller than or equal to the given key.
+// Floor key is defined as the greatest key that is less than or equal to the given key.
 // A floor key may not be found, either because the map is empty, or because
-// all keys in the map are larger than the given key.
-//
-// Key should adhere to the comparator's type assertion, otherwise method panics.
+// all keys in the map are greater than the given key.
 func (m *TreeMap[K, V]) Floor(key K) (foundKey K, foundValue V, ok bool) {
 	node, found := m.tree.Floor(key)
 	if found {
@@ -95,14 +92,11 @@ func (m *TreeMap[K, V]) Floor(key K) (foundKey K, foundValue V, ok bool) {
 }
 
 // Ceiling finds the ceiling key-value pair for the input key.
-// In case that no ceiling is found, then both returned values will be nil.
-// It's generally enough to check the first value (key) for nil, which determines if ceiling was found.
+// In case that no ceiling is found, then both returned values will be corresponding type's empty value.
 //
-// Ceiling key is defined as the smallest key that is larger than or equal to the given key.
+// Ceiling key is defined as the least key that is greater than or equal to the given key.
 // A ceiling key may not be found, either because the map is empty, or because
-// all keys in the map are smaller than the given key.
-//
-// Key should adhere to the comparator's type assertion, otherwise method panics.
+// all keys in the map are less than the given key.
 func (m *TreeMap[K, V]) Ceiling(key K) (foundKey K, foundValue V, ok bool) {
 	node, found := m.tree.Ceiling(key)
 	if found {
@@ -113,7 +107,7 @@ func (m *TreeMap[K, V]) Ceiling(key K) (foundKey K, foundValue V, ok bool) {
 
 // UpperBound returns an iterator pointing to the first element that is greater than key.
 // If no such element is found, a past-the-end iterator is returned.
-// This is a C++ style function of Floor.
+// See: https://en.cppreference.com/w/cpp/container/map/upper_bound
 func (m *TreeMap[K, V]) UpperBound(key K) TreeMapIterator[K, V] {
 	iter := m.tree.Iterator()
 	iter.Begin()
@@ -127,7 +121,7 @@ func (m *TreeMap[K, V]) UpperBound(key K) TreeMapIterator[K, V] {
 
 // LowerBound returns an iterator pointing to the first element that is not less than key.
 // If no such element is found, a past-the-end iterator is returned.
-// This is a C++ style function of Ceiling.
+// See: https://en.cppreference.com/w/cpp/container/map/lower_bound
 func (m *TreeMap[K, V]) LowerBound(key K) TreeMapIterator[K, V] {
 	iter := m.tree.Iterator()
 	iter.End()
