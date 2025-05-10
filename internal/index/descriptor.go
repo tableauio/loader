478beeb2a80897ec44b0b3fc790d0062a349f391
Kybxd <627940450@qq.com>
2025-05-10 14:28:49 +0800

    feat: add file path

--- internal/index/descriptor.go
+++ internal/index/descriptor.go
@@ -166,7 +166,7 @@ func ParseIndexDescriptor(gen *protogen.Plugin, md protoreflect.MessageDescripto
 	indexNameMap := map[string]int{}
 	for i, descriptor := range descriptors {
 		if j, ok := indexNameMap[descriptor.Name]; ok {
-			panic(fmt.Sprintf("duplicate index name on %v: %v and %v", md.Name(), indexes[j], indexes[i]))
+			panic(fmt.Sprintf("duplicate index name on %v in %v: %v and %v", md.Name(), md.ParentFile().Path(), indexes[j], indexes[i]))
 		} else {
 			indexNameMap[descriptor.Name] = i
 		}
