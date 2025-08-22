10ebc2c5be044d6d6855b1153ff689e4ef3d9b2f
Kybxd <627940450@qq.com>
2025-08-22 12:01:24 +0800

    feat: add notes

--- init.bat
+++ init.bat
@@ -12,6 +12,7 @@ git checkout v3.19.3
 git submodule update --init --recursive
 
 REM Build and install the C++ Protocol Buffer runtime and the Protocol Buffer compiler (protoc)
+REM Refer: https://github.com/protocolbuffers/protobuf/blob/3.19.x/cmake/README.md#cmake-configuration
 cd cmake
 mkdir build 2>nul
 cd build
